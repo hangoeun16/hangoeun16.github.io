2c211082704b09104ec24cc80f2db6e0f0590c6e
hangoeun16 <127250534+hangoeun16@users.noreply.github.com>
2025-12-12 19:28:57 +0000

    Deploying to gh-pages from @ hangoeun16/hangoeun16.github.io@f29763d6cda1227b6edb6a15d1c299190b0aff5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/lit_review/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-extracting-family-trees-from-literary-texts",
+        
+          title: "Extracting Family Trees from Literary Texts",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/extracting_family_trees_from_literary_texts/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
