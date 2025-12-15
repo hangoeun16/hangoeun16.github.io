77faae8d4009e31dca96530d16826b6aa4ef1efd
hangoeun16 <hangoeun16@users.noreply.github.com>
2025-12-15 17:53:12 +0000

    Deploying to gh-pages from @ hangoeun16/hangoeun16.github.io@286c31dafb9f43cf427bdc644227e8d0b6a3371d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,6 +427,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "lit_review-extracting-family-trees-from-literary-texts",
+          title: 'Extracting Family Trees from Literary Texts',
+          description: "",
+          section: "Lit_review",handler: () => {
+              window.location.href = "/lit_review/2025-12-12-extracting_family_trees_from_literary_texts/";
             },},{id: "lit_review-does-environmental-policy-affect-scaling-laws-between-population-and-pollution-evidence-from-american-metropolitan-areas",
           title: 'Does environmental policy affect scaling laws between population and pollution? Evidence from American...',
           description: "",
