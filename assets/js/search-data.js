151cbe75c01518d890907169aa25550782f3f2f9
hangoeun16 <127250534+hangoeun16@users.noreply.github.com>
2025-12-16 01:43:32 +0000

    Deploying to gh-pages from @ hangoeun16/hangoeun16.github.io@b1f257d3f93c736029c14539b52f5dd8327ffa82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -510,6 +510,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-the-number-of-order-2-elements-in-dihedral-group",
+          title: 'The Number of Order 2 Elements in Dihedral Group',
+          description: "proof for the number of order 2 elements in each dihedral group",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/dihedral_group/";
             },},{
         id: 'social-cv',
         title: 'CV',
