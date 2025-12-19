4cde82656fb4505f225d2447ecdb939fbdc48abe
hangoeun16 <127250534+hangoeun16@users.noreply.github.com>
2025-12-19 03:55:32 +0000

    Deploying to gh-pages from @ hangoeun16/hangoeun16.github.io@70d627383dc01f9895c130df585b2f1ded019ead 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,7 +436,7 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "courses-inm713-knowledge-graphs-in-the-era-of-large-language-models",
           title: 'INM713: Knowledge Graphs in the Era of Large Language Models',
-          description: "This is a PhD-level course delivered at the University of Aalborg. The course [Knowledge Graphs in the Era of Large Language Models](https://github.com/city-knowledge-graphs/phd-course?tab=readme-ov-file) is based on [City&#39;s Semantic Web Technologies and Knowledge Graphs module](https://github.com/turing-knowledge-graphs/teaching/tree/main/city). Since the exact course code for the Aalborg version was not available, I used the course code from City, University of London.Course information sourced from the Knowledge Graphs Interest Group at the Alan Turing Institute: [Teaching material relevant to KGs](https://github.com/turing-knowledge-graphs/teaching).",
+          description: "",
           section: "Courses",handler: () => {
               window.location.href = "/courses/INM713/description/";
             },},{id: "courses-introduction-to-knowledge-graphs",
