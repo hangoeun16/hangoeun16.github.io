c01b848ece74af1e3a8e7c2924734dde8778d818
hangoeun16 <127250534+hangoeun16@users.noreply.github.com>
2026-06-27 19:44:22 +0000

    Deploying to gh-pages from @ hangoeun16/hangoeun16.github.io@6798304fb741d553cbcf4ffa19558abf88a1a8ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,17 +121,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/pseudocode/";
           
         },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
       },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
         
           title: "displaying beautiful tables with Bootstrap Tables",
